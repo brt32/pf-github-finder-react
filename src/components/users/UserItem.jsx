7ad1b64664a5c7c12e6eac78b73ea1acced4e085
Bartek <bartlomiej.proczkowski@gmail.com>
2022-01-14 22:41:27 +0100

    Initial commit

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -16,9 +16,9 @@ function UserItem({ user: { login, avatar_url } }) {
           <h2 className="card-title">{login}</h2>
           <Link
             className="text-base-content text-opacity-40"
-            to={`/users/${login}`}
+            to={`/user/${login}`}
           >
-            Play
+            Visit Profile
           </Link>
         </div>
       </div>
